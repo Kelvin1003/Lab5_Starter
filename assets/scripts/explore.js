86445559746bf5ae4dcb221a4cccf04406a3b79a
Kelvin1003 <kay003@ucsd.edu>
2021-10-22 08:28:26 -0700

    test voice

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,31 @@
 // explore.js
 
+const $ = (selector) => document.querySelector(selector);
+
+let voiceList;
+
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  voiceList = $('#voice-select');
+  setTimeout(() => {
+    console.log(window.speechSynthesis.getVoices());
+}, 100);
+  // showVoiceList();
 }
+
+function showVoiceList() {
+  let voices = window.speechSynthesis.getVoices();
+  setTimeout(() => {
+    console.log(voices);
+    for (let i = 0; i < voices.length ; i++) {
+      let newOption = document.createElement('option');
+      newOption.textContent = `${voices[i].name} (${voices[i].lang})`;
+      newOption.setAttribute('value', voices[i].name);
+      voiceList.appendChild(newOption);
+    }
+  }, 2000);
+
+}
+
